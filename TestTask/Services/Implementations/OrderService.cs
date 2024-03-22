91dafab38ab5309c04a1f0994ac32379d15c033f
Kerfl <Kerfl@DESKTOP-18CDLEI>
2024-03-22 18:02:34 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public OrderService(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<Order> GetOrder()
+        {
+            var priceMax = await _applicationDbContext.Orders.MaxAsync(order => order.Price);
+            var orderPriceMax = await _applicationDbContext.Orders.FirstOrDefaultAsync(order => order.Price == priceMax);
+            return orderPriceMax;
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            const int orderQuantity = 10;
+            var orders = await _applicationDbContext.Orders.Where(order => order.Quantity >= orderQuantity).ToListAsync<Order>();
+            return orders;
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,36 @@
+﻿using TestTask.Models;
+using TestTask.Services.Interfaces;
+using TestTask.Data;
+using Microsoft.EntityFrameworkCore;
+using TestTask.Enums;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public UserService(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<User> GetUser()
+        {
+            var user = await _applicationDbContext.Orders.GroupBy(order => order.UserId)
+                                                         .Select(order => new { UserId = order.Key, OrderCount = order.Count() })
+                                                         .OrderByDescending(order => order.OrderCount)
+                                                         .FirstOrDefaultAsync();
+
+            var userMax = await _applicationDbContext.Users.FirstOrDefaultAsync(users => users.Id == user.UserId);
+            return userMax;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            var usersInactive = await _applicationDbContext.Users.Where(user => user.Status == UserStatus.Inactive)
+                                                                 .ToListAsync<User>();
+            return usersInactive;
+        }
+    }
+}
